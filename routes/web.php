b030c92b9f71639d77d52e0a3f72b75e1ecf34d4
Suh Edmond <suhedmond25@yahoo.com>
2021-01-11 01:00:24 +0100

    Added routes to the different views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("saving_types", [App\Http\Controllers\SavingTypeController::class, 'index']); //view all available saving types
+
+Route::get("saving_types/create", [App\Http\Controllers\SavingTypeController::class, 'create']); ///form to add a new challenge
+Route::post("saving_types", [App\Http\Controllers\SavingTypeController::class, 'store']); ///add a new saving type
+Route::get("saving_types/{saving_type}", [App\Http\Controllers\HasSavingsController::class, 'store']); //add a saving type to my selected saving types
+Route::get("savings", [App\Http\Controllers\SavingsController::class, 'index']); //view all selected saving types
+Route::get("savings/{id}/add", [App\Http\Controllers\SavingsController::class, 'create']); //add saving
+Route::post("savings/{id}/", [App\Http\Controllers\SavingsController::class, 'store']); //store savings
+Route::get("savings/{id}/", [App\Http\Controllers\SavingsController::class, 'show']); //view all savings for a particular saving type
+Route::delete("savings/{id}/", [App\Http\Controllers\SavingsController::class, 'destroy']);
